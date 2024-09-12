04fec73d544f108b59db65dcd36613e3f20fa6fb
bangiscoder <bangiscoder@gmail.com>
2024-09-12 03:24:27 +0100

    Adding an I frame to embed fellows names

--- index.php
+++ index.php
@@ -9,5 +9,6 @@
     <?php
         echo "<h1>Hello Fellows</h1>"
     ?>
+    <iframe src="https://github.com/bangiscoder/3mttblog/blob/main/fellows.html" frameborder="0"></iframe>
 </body>
 </html>
